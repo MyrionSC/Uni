43b5095b0b28f22fc95f6a8161d1d2daef017052
myrionsc <marpht23@gmail.com>
2017-10-10 15:22:46 +0200

    haskell

--- pp/assignment1/main.rkt
+++ pp/assignment1/main.rkt
@@ -4,7 +4,7 @@
 ;test data
 (define notelist (list (note-abs-time-with-duration 0 1 60 80 1920) (note-abs-time-with-duration 1920 1 64 80 960) (note-abs-time-with-duration 2880 1 62 80 1920) (note-abs-time-with-duration 4800 1 64 80 960) (note-abs-time-with-duration 5760 1 60 80 1920) (note-abs-time-with-duration 7680 1 59 80 960) (note-abs-time-with-duration 8640 1 57 80 1920) (note-abs-time-with-duration 10560 1 60 80 960) (note-abs-time-with-duration 11520 1 64 80 1920) (note-abs-time-with-duration 13440 1 64 80 960) (note-abs-time-with-duration 14400 1 64 80 1920) (note-abs-time-with-duration 16320 1 60 80 960) (note-abs-time-with-duration 17280 1 57 80 3840)))
 
-; <your definitions here>
+
 
 
 
---
+++ pp/haskell/haskproj/app/Main.hs
@@ -0,0 +1,39 @@
+module Main where
+import Lib
+
+---- lecture 1
+
+-- subject 3
+
+myrev :: [any] -> [any]
+myrev a = reverse a
+
+-- subject 6
+
+lastL lst = if length lst == 1 then head lst else lastL lst
+
+-- subject 7
+
+flatten :: [[any]] -> [any]
+flatten [] = []
+flatten (x:xs) = x ++ flatten xs
+
+
+
+--duplfirst (x:xs) = x:x:xs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main = print "done"
