655d8b4b289028c81a404cca1afa17776c78c3b7
myrionsc <marpht23@gmail.com>
2017-10-12 22:28:26 +0200

    music duration function done

--- pp/assignment1/main.rkt
+++ pp/assignment1/main.rkt
@@ -9,6 +9,8 @@
           (get-item (cdr lst) (- n 1)))))
 (define (! pred)
   (if (eqv? pred #t) #f #t))
+(define head car)
+(define tail cdr)
 
 ;;; global variables
 (define piano 0)
@@ -19,32 +21,21 @@
 (define trumpet 5)
 (define helicopter 6)
 (define telephone 7)
-
 (define sec 960)
 (define eight (/ sec 8))
 (define quarter (/ sec 4))
 (define half (/ sec 2))
 (define full sec)
+(define velo 80)
 
 
-;;; model
-
-;; music element
-;;   - Note (pitchvalue, duration, instrument)
-;;   - pause (duration)
-;;   - sequentialMusicElement (musicElements)
-;;   - parallelMusicElement (musicElements)
-
-;; pitchvalue: int between 0 and 127
-;; duration: timeunit where 960 is a second
-;; instruments: Piano, Organ, Guitar, Violin, Flute, Trumpet, Helicopter, Telephone
 
 ;;; 1: create constructors for music elements
 
-;;; create super element
+;; create super element
 (struct music-element (type els props))
 
-;;; create specific elements
+;; create specific elements
 (define (note pit dur ins)
   (music-element 'note '() (list pit dur ins)))
 (define (pause dur)
@@ -58,12 +49,10 @@
 (define (music-element-pred? type)
   (lambda (me)
     (and (music-element? me) (eqv? (music-element-type me) type))))
-
 (define note? (music-element-pred? 'note))
 (define pause? (music-element-pred? 'pause))
 (define seq? (music-element-pred? 'seq))
 (define par? (music-element-pred? 'par))
-
 
 ;;; 3: selectors
 (define (get-note-item index)
@@ -73,20 +62,25 @@
 (define get-note-pit (get-note-item 0))
 (define get-note-dur (get-note-item 1))
 (define get-note-ins (get-note-item 2))
-
 (define (get-pause-dur p)
     (if (pause? p) (get-item (music-element-props p) 0)
         (raise "type not pause" #t)))
-
 (define (get-els me)
   (if (or (seq? me) (par? me)) (music-element-els me)
       (raise "element not sequence or paralel music element")))
 
 
-
 ;;; 4: scale, transpose and reinstrument functions
 
 ;;; 5: duration of music element function
+(define (get-music-dur me)
+    (cond ((eqv? me '()) 0)
+          ((note? me) (get-note-dur me)) ;transform to note-abs
+          ((pause? me) (get-pause-dur me)) ;get duration somehow
+          ((seq? me) (get-music-dur (get-els me))) ;call recursively on all elements
+          ((par? me) (get-music-dur (get-els me)))
+          ((if (par? (head me)) (max (get-music-dur (head me)) (get-music-dur (tail me)))
+               (+ (get-music-dur (head me)) (get-music-dur (tail me)))))))
 
 ;;; 6: is monophonic function
 
@@ -96,19 +90,30 @@
 
 (define (transform-to-note-abs-time-with-duration me)
   (cond ((! (music-element? me)) (raise "input not music element" #t))
-        
-        ;; should be in helper
-        ((note? me) "note") ;transform to note-abs
-        ((pause? me) "pause") ;ignore
-        ((seq? me) "seq") ;call recursively on all elements
-        ("par"))) ;call recursively on all elements
-
-;transform-to-note-abs-time-with-duration-helper (me, dur, res)
+        ((cdr (transform-helper me 0)))))
 
 
+(define (transform-helper me dur)
+  (cond ((note? me) (cons (+ dur (get-note-dur me)) '())) ;transform to note-abs
+        ((pause? me) "pause") ;get duration somehow
+        ((seq? me) "seq")  ;call recursively on all elements
+        ((par? me) "par")
+        ("maybe list?")))
+;call recursively on all elements
 
+
+;; should be in helper
+        ;((note? me) "note") ;transform to note-abs
+        ;((pause? me) "pause") ;ignore
+        ;((seq? me) "seq") ;call recursively on all elements
+        ;("par"))) ;call recursively on all elements
 
 ;;; test
+(define n3 (note 60 half piano))
+(define tel (par (list (note 60 half piano) (pause half) (note 60 half piano) (seq (list (note 60 full piano) (pause quarter))) (par (list (note 60 full piano) (note 60 full piano) (pause quarter))))))
+;(get-music-dur tel)
+
+
 (define n1 (note 1 2 3))
 (define p1 (pause 123))
 (define s1 (seq (list n1 p1)))
@@ -126,9 +131,17 @@
 ;; instruments: Piano, Organ, Guitar, Violin, Flute, Trumpet, Helicopter, Telephone
 
 ;;; canon
+(define test (note 60 half piano))
+(define testelement (seq (list (note 60 half piano) (pause half) (note 60 half piano))))
 (define peter-jakob (seq (list (note 60 half piano) (pause half) (note 60 half piano) (pause half) (note 60 half piano) (pause half) (note 60 half piano) (pause half) (note 60 half piano))))
-(transform-to-note-abs-time-with-duration peter-jakob)
+;(transform-to-note-abs-time-with-duration test)
 
+;abs-time:	A non-negative integer, in time ticks. The absolute start time of the note.
+;channel:	An integer between 1 and 16. The MIDI channel allocated to the requested instrument. Se the miniproject formulation for more details.
+;note-number:	An integer between 0 and 127. The MIDI note number.
+;velocity:	An integer between 0 and 127. The velocity (strength) of the note. Not controllable in this exercise. It can be a constant, such as 80.
+;duration:	A non-negative integer. The duration of this note (a number of time ticks).
+ 
 
 ;(define notelist (list (note-abs-time-with-duration 0 1 60 80 1920) (note-abs-time-with-duration 1920 1 64 80 960) (note-abs-time-with-duration 2880 1 62 80 1920) (note-abs-time-with-duration 4800 1 64 80 960) (note-abs-time-with-duration 5760 1 60 80 1920) (note-abs-time-with-duration 7680 1 59 80 960) (note-abs-time-with-duration 8640 1 57 80 1920) (note-abs-time-with-duration 10560 1 60 80 960) (note-abs-time-with-duration 11520 1 64 80 1920) (note-abs-time-with-duration 13440 1 64 80 960) (note-abs-time-with-duration 14400 1 64 80 1920) (note-abs-time-with-duration 16320 1 60 80 960) (note-abs-time-with-duration 17280 1 57 80 3840)))
 ;(transform-to-midi-file-and-write-to-file! notelist "generated-music.mid")
