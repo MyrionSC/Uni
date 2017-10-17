c2221bca3188a05b58335549fda4809afcd921bf
myrionsc <marpht23@gmail.com>
2017-10-17 13:47:38 +0200

    some more canon :)

--- pp/assignment1/main.rkt
+++ pp/assignment1/main.rkt
@@ -11,6 +11,10 @@
   (if (eqv? pred #t) #f #t))
 (define head car)
 (define tail cdr)
+(define (p val) ;prints a value
+  (display val)
+  (newline)
+  val)
 
 ;;; global variables
 (define piano 1)
@@ -91,13 +95,7 @@
 
 ;;; 8: transform to list of note-abs-time-with-duration
 
-(define (pause-to-natwd abstime)
-  (display abstime)
-  (newline)
-  '())
-
 (define (note-to-natwd n abstime)
-  (display abstime) (newline)
   (note-abs-time-with-duration abstime (get-note-ins n) (get-note-pit n) 80 (get-note-dur n)))
 
 (define (transform-to-note-abs-time-with-duration me)
@@ -107,49 +105,26 @@
 (define (transform-helper me abstime)
   (cond ((eqv? me '()) '())
         ((note? me) (list (note-to-natwd me abstime)))
-        ((pause? me) (pause-to-natwd abstime))
+        ((pause? me) '())
         ((or (seq? me) (par? me)) (transform-helper (get-els me) abstime))
-        ((append (transform-helper (head me) abstime) (transform-helper (tail me) (+ abstime (get-music-dur (head me))))))))
-        
-        ;((if (seq? (head me)) (append (transform-helper (head me) abstime) (transform-helper (tail me) (+ abstime (get-music-dur (head me)))))
-             ;(append (transform-helper (head me) abstime) (transform-helper (tail me) abstime))))))
+        ((if (par? (head me)) (append (transform-helper (head me) abstime) (transform-helper (tail me) abstime))
+             (append (transform-helper (head me) abstime) (transform-helper (tail me) (+ abstime (get-music-dur (head me)))))))))
 
-;;; test
-(define n1 (note 1 2 3))
-(define p1 (pause 123))
-(define s1 (seq (list n1 p1)))
-(define par1 (par (list s1)))
-(define text "test")
-
-(define t1 (note-to-natwd n1 1))
-(define t2 (note-to-natwd n1 2))
-(define l1 (list t1 t1))
-;t1
-;l1
-(append l1 (list t2))
-
-(define testelement (seq (list (note 60 half piano) (pause half) (note 60 half piano))))
-(define testseq (seq (list testelement (par (list testelement)) (pause 100) testelement)))
-
-"how it looks now"
-(transform-to-note-abs-time-with-duration testelement)
+;(define testelement (seq (list (note 60 half piano) (pause half) (seq (list (par (list (note 60 half piano))) (note 60 half piano))))))
+;(define testelement (seq (list (note 60 half piano) (pause half) (note 60 half piano))))
+;(define testseq (seq (list testelement (par (list testelement)) (pause 100) testelement)))
 
 
-"how it should look"
+;"how it should look"
 ;(define notelist (list (note-abs-time-with-duration 0 1 60 80 1920) (note-abs-time-with-duration 1920 1 64 80 960) (note-abs-time-with-duration 2880 1 62 80 1920) (note-abs-time-with-duration 4800 1 64 80 960) (note-abs-time-with-duration 5760 1 60 80 1920) (note-abs-time-with-duration 7680 1 59 80 960) (note-abs-time-with-duration 8640 1 57 80 1920) (note-abs-time-with-duration 10560 1 60 80 960) (note-abs-time-with-duration 11520 1 64 80 1920) (note-abs-time-with-duration 13440 1 64 80 960) (note-abs-time-with-duration 14400 1 64 80 1920) (note-abs-time-with-duration 16320 1 60 80 960) (note-abs-time-with-duration 17280 1 57 80 3840)))
-(define notelist (list (note-abs-time-with-duration 0 1 60 80 1920) (note-abs-time-with-duration 1920 1 64 80 960) (note-abs-time-with-duration 2880 1 62 80 1920)))
-notelist
+;(define notelist (list (note-abs-time-with-duration 0 1 60 80 1920) (note-abs-time-with-duration 1920 1 64 80 960) (note-abs-time-with-duration 2880 1 62 80 1920)))
+;notelist
 ;(transform-to-midi-file-and-write-to-file! notelist "generated-music.mid")
 
 
 
 
-
-
-
-
-;;; canon
-(define my-melody (seq  (list
+(define mester-jakob (seq (list
                            (note 65 480 piano)
                            (note 67 480 piano)
                            (note 69 480 piano)
@@ -157,31 +132,49 @@ notelist
                            (note 65 480 piano)
                            (note 67 480 piano)
                            (note 69 480 piano)
-                           (note 65 480 piano)
-                           (note 69 480 piano)
-                           (note 70 480 piano)
-                           (note 72 960 piano)
-                           (note 69 480 piano)
-                           (note 70 480 piano)
-                           (note 72 960 piano)
-                           (note 72 240 piano)
-                           (note 74 240 piano)
-                           (note 72 240 piano)
-                           (note 70 240 piano)
-                           (note 69 480 piano)
-                           (note 65 480 piano)
-                           (note 72 240 piano)
-                           (note 74 240 piano)
-                           (note 72 240 piano)
-                           (note 70 240 piano)
-                           (note 69 480 piano)
-                           (note 65 480 piano)
-                           (note 65 480 piano)
-                           (note 60 480 piano)
-                           (note 65 960 piano)
-                           (note 65 480 piano)
-                           (note 60 480 piano)
-                           (note 65 960 piano))))
+                           (note 65 480 piano))))
+(define hvor-er-du (seq (list
+                         (note 69 480 piano)
+                         (note 70 480 piano)
+                         (note 72 960 piano)
+                         (note 69 480 piano)
+                         (note 70 480 piano)
+                         (note 72 960 piano))))
+(define ringer-du-med-klokken (seq (list
+                                    (note 72 240 piano)
+                                    (note 74 240 piano)
+                                    (note 72 240 piano)
+                                    (note 70 240 piano)
+                                    (note 69 480 piano)
+                                    (note 65 480 piano)
+                                    (note 72 240 piano)
+                                    (note 74 240 piano)
+                                    (note 72 240 piano)
+                                    (note 70 240 piano)
+                                    (note 69 480 piano)
+                                    (note 65 480 piano))))
+(define bim-bam-bum (seq (list
+                          (note 65 480 piano)
+                          (note 60 480 piano)
+                          (note 65 960 piano)
+                          (note 65 480 piano)
+                          (note 60 480 piano)
+                          (note 65 960 piano))))
+(define song (seq (list mester-jakob hvor-er-du ringer-du-med-klokken bim-bam-bum)))
+(define canon (seq (list
+                    mester-jakob
+                    (par (list song))
+                    hvor-er-du
+                    (par (list song))
+                    ringer-du-med-klokken
+                    (par (list song))
+                    bim-bam-bum)))
+
+
+(define abscanon (transform-to-note-abs-time-with-duration canon))
+(transform-to-midi-file-and-write-to-file! abscanon "mester-jakob.mid")
+
+
 ;my-melody
 ;(define mester-jakob (transform-to-note-abs-time-with-duration my-melody))
 ;mester-jakob
