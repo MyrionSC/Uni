ab776f0661b64407255938c43d77af7f4bd2159f
myrionsc <marpht23@gmail.com>
2017-11-15 22:05:22 +0100

    added pp miniproject 2 start

---
+++ pp/haskell/mini-project-two/app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Lib
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main :: IO ()
+main = someFunc
+
+-- https://www.moodle.aau.dk/pluginfile.php/1065071/mod_label/intro/miniproject-2.pdf
+-- http://people.cs.aau.dk/~hans/miniproject-2.html
+-- https://www.haskell.org/happy/
