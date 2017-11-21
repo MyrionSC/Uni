a9782544fce290fe0ab272850f4c3f025dd76d48
myrionsc <marpht23@gmail.com>
2017-11-21 11:55:40 +0100

    added numbers to test

--- pp/haskell/minproj-two/src/Main.hs
+++ pp/haskell/minproj-two/src/Main.hs
@@ -7,13 +7,11 @@ runEvalWith :: (String -> Expr) -> String -> IO ()
 runEvalWith parseExpr input = do
   let ast = parseExpr input
   putStrLn $ "AST: " ++ (show ast)
-  putStrLn $ "Source: " ++ (source ast)
-  putStrLn $ "Eval: " ++ " not implemtented"
 
 main :: IO ()
 main = do
   input <- getContents
   putStrLn "Input:"
   putStrLn input
-  putStrLn "\nUsing Happy:"
   runEvalWith HappyParser.parseExpr input
+  putStrLn ""
