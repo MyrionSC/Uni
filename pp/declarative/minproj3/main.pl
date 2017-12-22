a67ac796bfcf788d85e26eb7b74a66d96a2ba57e
myrionsc <marpht23@gmail.com>
2017-12-22 17:18:08 +0100

    facts and predicates done

--- pp/declarative/minproj3/main.pl
+++ pp/declarative/minproj3/main.pl
@@ -1,61 +1,78 @@
 
 % --- Problem 1. Introduce appropriate predicates for the entities in the system.
-
-% airline(name).
-% airport(code, city, country).
-% aircraft(reg, owner, modelName).
-% model(name, class, manufactorer).
-% seat(aircraft, number, class, type).
-% adjacantSeat(seat1, seat2).
-% passenger(first, last, birthday).
-% passport(owner, country).
-% leg(origin, destination, servicer, aircraft).
-% reservation(code, passenger, origin, destination, airline, seatNumber)
-% itinerary(code, reservationCode).
-% viseAgreement(countryA, countryB).
-
-
 % --- Problem 2. Introduce facts for your choosen predicates.
 
+weather(Weather) :- string(Weather).
+weather(clear).
+weather(cloudy).
+weather(stormy).
+weather(thunderstorm).
+
+airline(Name) :- string(Name).
 airline(sas).
 airline(norwegian).
 
-airport(aal, aalborg, denmark).
-airport(lon, london, england).
-airport(agb, augsburg, germany).
-airport(ruh, riyadh, "Saudi Arabia").
+airport(Code, Country, Weather) :- string(Code), string(Country), weather(Weather).
+airport(aal, denmark).
+airport(lon, england).
+airport(agb, germany).
+airport(ruh, "Saudi Arabia").
 
+% aircraft(reg, owner, modelName).
+% seat(aircraft, number, class, type).
+% adjacantSeat(seat1, seat2).
+aircraft(Reg, Owner, Model) :- number(Reg), string(Owner), string(Model).
 aircraft(1, sas, cesna).
+aircraft(2, norwegian, "Airbus A380").
+seat(Reg, SeatNumber, Class, Type) :- number(Reg), string(SeatNumber), string(Class), string(Type).
 seat(1, "1A", economy, window).
 seat(1, "1B", economy, aisle).
-adjacantSeat(1, "1A", "1B");
-
-aircraft(2, norwegian, "Airbus A380").
 seat(2, "1A", business, window).
 seat(2, "1B", business, other).
 seat(2, "1C", business, aisle).
+adjacantSeat(Reg, Seat1, Seat2) :- number(Reg), string(Seat1), string(Seat2).
+adjacantSeat(1, "1A", "1B").
 adjacantSeat(2, "1A", "1B").
 adjacantSeat(2, "1B", "1C").
 
+model(Name, Class, Manufactorer) :- string(Name), string(Class), string(Manufactorer).
 model(cesna, light, "Airbus Industrie").
 model("Airbus A380", heavy, "Textron Aviation").
 
-passenger(dario, wunsch, "13-7-1990").
-passenger(jens, aasgaard, "28-7-1990").
-passenger(grzegorz, komincz, "14-12-1993").
-passenger(patrick, brix, "4-12-1992").
+passenger(Id, First, Last, Birthday) :- number(Id), string(First), string(Last), string(Birthday).
+passenger(1, dario, wunsch, "13-7-1990").
+passenger(2, jens, aasgaard, "28-7-1990").
+passenger(3, grzegorz, komincz, "14-12-1993").
+passenger(4, patrick, brix, "4-12-1992").
 
-passport(dario, wunsch, germany).
-passport(jens, aasgaard, norway).
-passport(grzegorz, komincz, poland).
-passport(patrick, brix, denmark).
-passport(patrick, brix, sweden).
+passport(Owner, Country) :- number(Owner), string(Country).
+passport(1, germany).
+passport(2, norway).
+passport(3, poland).
+passport(4, denmark).
+passport(4, sweden).
 
+leg(Origin, Destination, Servicer, Aircraft) :- string(Origin), string(Destination), string(Servicer), number(Aircraft).
+leg(aal, lon, sas, 1).
+leg(lon, agb, sas, 2).
+leg(augs, ruh, norwegian, 1).
+leg(ruh, aal, norwegian, 2).
 
+reservation(Code, Passenger, Origin, Destination, Airline, SeatNumber) :- string(Code), number(Passenger), string(Origin), string(Destination), string(Airline), string(SeatNumber).
+reservation("R2D2", 1, aal, agb, sas, 2, "1A").
+reservation("BB8", 1, agb, lon, sas, 1, "1C").
+reservation("C3PO", 2, ruh, lon, norwegian, 1, "1A").
+reservation("IG88", 3, ruh, lon, norwegian, 1, "1A").
 
-weather(clear).
-weather(cloudy).
-weather(thunderstorm).
+itinerary(Code, ReservationCode) :- string(Code), string(ReservationCode).
+itinerary("010", "R2D2").
+itinerary("010", "BB8").
+
+visaAgreement(CountryA, CountryB) :- string(CountryA), string(CountryB).
+visaAgreement(denmark, germany).
+visaAgreement(germany, england).
+visaAgreement(england, denmark).
+
 
 
 
