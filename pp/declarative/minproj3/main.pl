c45d43fc0621b88b792424421cf2dc49390ab9a5
myrionsc <marpht23@gmail.com>
2017-12-23 14:18:08 +0100

    removed airline from reservation (aircraft is enough)

--- pp/declarative/minproj3/main.pl
+++ pp/declarative/minproj3/main.pl
@@ -12,7 +12,7 @@ airport(lon, england, thunderstorm).
 airport(ruh, "Saudi Arabia", cloudy).
 airport(aal, denmark, clear).
 
-aircraft(Reg, Owner, Model) :- number(Reg), string(Owner), string(Model).
+aircraft(Reg, Owner, Model) :- number(Reg), airline(Owner), string(Model).
 aircraft(1, sas, cesna).
 aircraft(2, norwegian, "Airbus A380").
 seat(Reg, SeatNumber, Class, Type) :- number(Reg), string(SeatNumber), string(Class), string(Type).
@@ -27,8 +27,8 @@ adjacantSeat(2, "1A", "1B").
 adjacantSeat(2, "1B", "1C").
 
 model(Name, Class, Manufactorer) :- string(Name), string(Class), string(Manufactorer).
-model(cesna, light, "Airbus Industrie").
-model("Airbus A380", heavy, "Textron Aviation").
+model(cesna, light, "Textron Aviation").
+model("Airbus A380", heavy, "Airbus Industrie").
 
 passenger(Id, First, Last, Birthday) :- number(Id), string(First), string(Last), string(Birthday).
 passenger(1, dario, wunsch, "13-7-1990").
@@ -49,12 +49,12 @@ leg(lon, agb, sas, 2).
 leg(augs, ruh, norwegian, 1).
 leg(ruh, aal, norwegian, 2).
 
-reservation(Code, Passenger, Origin, Destination, Airline, SeatNumber) :- string(Code), number(Passenger), string(Origin), string(Destination), string(Airline), string(SeatNumber).
-reservation("R2D2", 1, aal, agb, sas, 2, "1A").
-reservation("BB8", 1, agb, lon, sas, 1, "1C").
-reservation("C3PO", 2, lon, aal, norwegian, 1, "1A").
-reservation("IG88", 3, lon, aal, norwegian, 1, "1A"). % double reservation % illegal reservation
-reservation("BB9E", 4, aal, ruh, norwegian, 2, "1B").
+reservation(Code, Passenger, Origin, Destination, Aircraft, SeatNumber) :- string(Code), number(Passenger), string(Origin), string(Destination), string(Aircraft), string(SeatNumber).
+reservation("R2D2", 1, aal, agb, 2, "1A").
+reservation("BB8", 1, agb, lon, 1, "1C").
+reservation("C3PO", 2, lon, aal, 1, "1A").
+reservation("IG88", 3, lon, aal, 1, "1A"). % double reservation % illegal reservation
+reservation("BB9E", 4, aal, ruh, 2, "1B").
 
 itinerary(Code, ReservationCode) :- string(Code), string(ReservationCode).
 itinerary("010", "R2D2").
@@ -90,8 +90,8 @@ mayFlyTo(PassengerId, AirportCode) :- passport(PassengerId, Origin),
 %she is not permitted to enter the country of the destination airport.
 % --- Problem 4. Compute the passengers that have illegal reservations.
 
-legalReservations(Pid, Rid, Dest) :- reservation(Rid, Pid, _, Dest, _, _, _), mayFlyTo(Pid, Dest).
-illegalReservations(Pid) :- reservation(Rid, Pid, _, Dest, _, _, _), not(legalReservations(Pid, Rid, Dest)).
+legalReservations(Pid, Rid, Dest) :- reservation(Rid, Pid, _, Dest, _, _), mayFlyTo(Pid, Dest).
+illegalReservations(Pid) :- reservation(Rid, Pid, _, Dest, _, _), not(legalReservations(Pid, Rid, Dest)).
 
 %\+((treeHeight(T, Y), Y > X))
 
@@ -99,7 +99,10 @@ illegalReservations(Pid) :- reservation(Rid, Pid, _, Dest, _, _, _), not(legalRe
 %reserved by two different passe
 % --- Problem 5. Compute the booking code of all double bookings.
 
-
+doubleBookings(Pid, Rid) :- reservation(Rid, Pid, _, _, _, _).
+%doubleBookings(Rid) :- reservation(Rid, Pid, Origin, Destination, Aircraft, SeatNumber).
+%doubleBookings(Rid) :- reservation(Rid, _, Origin, Destination, Airline, SeatNumber),
+%                       reservation(Rid2, _, Origin, Destination, Airline, SeatNumber).
 
 
 
