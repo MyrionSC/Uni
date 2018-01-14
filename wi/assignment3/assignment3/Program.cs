02203ccaeac950ea59d43edafe6e9abec0456b5a
myrionsc <marpht23@gmail.com>
2018-01-15 00:03:58 +0100

    pearsons seems to be working

--- wi/assignment3/assignment3/Program.cs
+++ wi/assignment3/assignment3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using assignment3.model;
+using Microsoft.Win32;
 
 namespace assignment3
 {
@@ -15,11 +16,11 @@ namespace assignment3
             List<Rating> testRatings = Parser.ParseRatings("data/u1.test");
 
             // create user-item rating matrix
+            int test = trainingRatings.Count;
             int usersCount = trainingRatings.Select(r => r.userId).ToArray().Max() + 1;
             int itemsCount = trainingRatings.Select(r => r.itemId).ToArray().Max() + 1;
             userMatrix = new int[usersCount, itemsCount];
-            trainingRatings.ForEach(r =>
-            {
+            trainingRatings.ForEach(r => {
                 userMatrix[r.userId, r.itemId] = r.rating;
             });
 
@@ -27,16 +28,16 @@ namespace assignment3
             // 2: Find nearest neighbors to user
             // 3: combine ratings of neighbors to predict ratings
 
-            
-            
-            
-            
-            
-//            Console.WriteLine(userSimilarity(1, 13));
+//            HashSet<int> neighbors = findNeigbors(1, usersCount);
+
+
+
+
+            Console.WriteLine(userSimilarity(1, 1));
 
 //            double curmax = 0;
 //            int mostSim = 0;
-//            for (int i = 1; i < 50; i++)
+//            for (int i = 1; i < usersCount; i++)
 //            {
 //                if (i == 1) continue;
 //                
@@ -51,7 +52,23 @@ namespace assignment3
 //            Console.WriteLine();
 //            Console.WriteLine(curmax);
 //            Console.WriteLine(mostSim);
-            
+
+        }
+
+        public static HashSet<int> findNeigbors(int user, int usersCount)
+        {
+            HashSet<int> neighbors = new HashSet<int>();
+            for (int i = 1; i < usersCount; i++)
+            {
+                if (i == user) continue;
+                
+                var sim = userSimilarity(1, i);
+                if (sim > 0.5)
+                {
+                    neighbors.Add(i);
+                }
+            }
+            return neighbors;
         }
 
         public static double pred(int user, int item)
@@ -64,7 +81,7 @@ namespace assignment3
             return 1;
         }
 
-        public static double weightOfNeighbor(int user, int neighbor);
+        public static double weightOfNeighbor(int user, int neighbor)
         {
             //𝒘 (𝒂, 𝒃) = 𝒔𝒊𝒎(𝒂, 𝒃) / sum 𝒃∈𝑵 (𝒔𝒊𝒎(𝒂, 𝒃))
 //            var res = userSimilarity(user, neighbor) / userMatrix.
@@ -77,34 +94,35 @@ namespace assignment3
         {
             var aItems = userRatedItems(userA);
             var bItems = userRatedItems(userB);
-            var intersect = aItems.Intersect(bItems);
+            var intersect = new HashSet<int>(aItems.Intersect(bItems));
             var aAverage = averageRating(userA);
             var bAverage = averageRating(userB);
 
             double top = 0, bottomLeft = 0, bottomRight = 0;
             foreach (int item in intersect)
             {
-                top += (userRatingForItem(userA, item) - aAverage) * (userRatingForItem(userB, item) - bAverage);
-                bottomLeft += Math.Pow(userRatingForItem(userA, item), 2);
-                bottomRight += Math.Pow(userRatingForItem(userB, item), 2);
+                var topLeft = userRatingForItem(userA, item) - aAverage;
+                var topRight = userRatingForItem(userB, item) - bAverage;
+                top += topLeft * topRight;
+                bottomLeft += Math.Pow(userRatingForItem(userA, item) - aAverage, 2);
+                bottomRight += Math.Pow(userRatingForItem(userB, item) - bAverage, 2);
             }
-            double bottom = Math.Log(bottomLeft) * Math.Log(bottomRight);
-            var ret = top / bottom;
-            return ret;
+            double bottom = Math.Sqrt(bottomLeft) * Math.Sqrt(bottomRight);
+            
+            if (top == 0 && bottom == 0) return 0; // if no items in common
+            return top / bottom;
         }
         public static double averageRating(int user)
         {
             double ratingSum = 0;
-            double ratingCount = 0;
-            for (int i = 0; i < userMatrix.GetLength(1); i++)
+            double itemCount = 0;
+            
+            foreach (int item in userRatedItems(user))
             {
-                if (userMatrix[user, i] != 0)
-                {
-                    ratingSum += userRatingForItem(user, i);
-                    ratingCount++;
-                }
+                ratingSum += userRatingForItem(user, item);
+                itemCount++;
             }
-            return ratingSum / ratingCount;
+            return ratingSum / itemCount;
         }
         public static HashSet<int> userRatedItems(int user)
         {
