92518fa244df6426548d58aa8dc3d688b6593ceb
myrionsc <marpht23@gmail.com>
2018-01-16 01:11:45 +0100

    really now

---
+++ wi/wi-assignment-2/model/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace wi_assignment_2
+{
+    public class Review
+    {
+        public int Score;
+        public string Summary;
+        public string Text;
+
+        public Review()
+        {
+            Score = 0;
+            Summary = "";
+            Text = "";
+        }
+        public Review(int Score, string summary, string text)
+        {
+            this.Score = Score;
+            Summary = summary;
+            Text = text;
+        }
+    }
+}
