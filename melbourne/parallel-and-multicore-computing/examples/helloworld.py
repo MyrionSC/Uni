4c206c5c0908ce523477c88cbd0030494366f1cf
myrionsc <marpht23@gmail.com>
2018-08-02 09:37:38 +0845

    more sheit

---
+++ melbourne/parallel-and-multicore-computing/examples/helloworld.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from mpi4py import MPI
+import sys
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+print("Helloworld! I am process %d of %d.\n" % (rank, size))
