dedda531f465150234dc88883f28db58881c361a
myrionsc <marpht23@gmail.com>
2018-08-07 16:32:14 +0845

    work and play

---
+++ melbourne/parallel-and-multicore-computing/work/clion-first/cmake-build-debug/CMakeFiles/clion_first.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_first.dir/main.c.o"
+  "clion_first.pdb"
+  "clion_first"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clion_first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ melbourne/parallel-and-multicore-computing/work/clion-first/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    int i = 2;
+    printf("hey do %d", i);
+    return 0;
+}
